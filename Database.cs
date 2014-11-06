a2b66f253c15846fd5bd58f6915e40d1c4706b16
Nisha2192 <nisha2192@gmail.com>
2014-11-06 08:52:47 -0500

    commit17

---
+++ Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Configuration;
+
+public class Database
+{
+
+    public static String ConnectionString
+    {
+        get {
+            return WebConfigurationManager.
+                ConnectionStrings["appointmentsConnectionString"].
+                ConnectionString;
+
+        }
+    }
+}
