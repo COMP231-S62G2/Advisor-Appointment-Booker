d73e9632e3b46f9c708cc26c4be0b6e4700b3761
Nisha Dhanwani <nisha2192@gmail.com>
2014-12-07 18:18:07 -0500

    commitappdate

---
+++ listappointmentsbydate.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="listappointmentsbydate.aspx.cs" Inherits="listappointmentsbydate" Title="Appointment Date" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+     <div style="height:509px; width: 100%;">
+        <%= this.Scheduler.Render()%> // renders Scheduler to the page
+     </div>	
+</asp:Content>
+
+
---
+++ listappointmentsbydate.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using DHTMLX.Scheduler;
+
+public partial class listappointmentsbydate : System.Web.UI.Page
+{
+    public DHXScheduler Scheduler { get; set; }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Scheduler = new DHXScheduler();
+
+        Scheduler.InitialDate = new DateTime(2011, 11, 24);// the initial data of Scheduler
+
+        Scheduler.Config.first_hour = 8;//the minimum value of the hour scale
+        Scheduler.Config.last_hour = 19;//the maximum value of the hour scale
+        
+        Scheduler.Config.time_step = 30;//the scale interval for the time selector in the lightbox
+        Scheduler.Config.limit_time_select = true;// sets the max and min values for the time selector in the lightbox to the values of the last_hour and first_hour options
+
+        Scheduler.DataAction = this.ResolveUrl("~/Data.ashx");// the handler which defines loading data to Scheduler
+        Scheduler.SaveAction = this.ResolveUrl("~/Save.ashx");// the handler which defines create/update/delete logic
+        Scheduler.LoadData = true;
+        Scheduler.EnableDataprocessor = true;
+    }
+   
+}
